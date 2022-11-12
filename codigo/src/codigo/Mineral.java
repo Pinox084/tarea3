6d4a7955936510e6bcf929e94cda32d5dacf0b09
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2022-11-12 17:33:11 -0300

    Update Mineral.java

--- codigo/src/codigo/Mineral.java
+++ codigo/src/codigo/Mineral.java
@@ -1,6 +1,9 @@
 
 package codigo;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Mineral extends Bebida{
     public Mineral(int serie){
@@ -9,5 +12,10 @@ public class Mineral extends Bebida{
     @Override
     public String beber(){
         return super.beber()+"agua mineral";
+    }
+    @Override
+    public void paint(Graphics g,int x, int y){
+        g.setColor(Color.blue);
+        g.fillRect(x,y,28,18);
     }
 }

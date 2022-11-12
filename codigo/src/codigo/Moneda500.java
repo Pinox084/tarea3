2c72d2760a8795eb3b023e96d9fc370cb2d40ebd
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2022-11-12 17:35:34 -0300

    Update Moneda500.java

--- codigo/src/codigo/Moneda500.java
+++ codigo/src/codigo/Moneda500.java
@@ -1,11 +1,23 @@
 package codigo;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Moneda500 extends Moneda{
-    public Moneda500(int n){
-        super(n);
+    public Moneda500(int serie){
+        super(serie);
     }
     @Override
     public int getValor(){
         return super.getValor()+500;
     }
-    
+    @Override
+    public void paint(Graphics g,int x, int y){
+        g.setColor(Color.black);
+        g.fillOval(x,y,50,50);
+        g.setColor(Color.yellow);
+        g.fillOval(x,y,48,48);
+        g.setColor(Color.black);
+        g.drawString(""+this.getValor(), x+10, y+20);
+    }
 }

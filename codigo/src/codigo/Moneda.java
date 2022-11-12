547bc0208d7156aff53ab62cb43f917c838bf704
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2022-11-12 17:33:54 -0300

    Update Moneda.java

--- codigo/src/codigo/Moneda.java
+++ codigo/src/codigo/Moneda.java
@@ -1,6 +1,13 @@
 package codigo;
-public abstract class Moneda {
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+public abstract class Moneda extends JPanel{
     private int serie;
+    private Graphics g;
     public Moneda(int serie){
         this.serie = serie;
     }
@@ -9,5 +16,10 @@ public abstract class Moneda {
     }
     public int getValor(){
         return 0;
+    }
+    public void paint(Graphics g,int x, int y){
+        g.setColor(Color.yellow);
+        g.fillOval(x,y,30,30);
+        
     }
 }

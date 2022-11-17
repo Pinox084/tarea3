6ae60deb031f919b872d6aeb26493a36625b6667
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2022-11-17 19:49:39 -0300

    setXY

--- codigo/src/codigo/Moneda500.java
+++ codigo/src/codigo/Moneda500.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Moneda500 extends Moneda{
+    private int X;
+    private int Y;
     public Moneda500(int serie){
         super(serie);
     }
@@ -12,12 +14,16 @@ public class Moneda500 extends Moneda{
         return super.getValor()+500;
     }
     @Override
-    public void paint(Graphics g,int x, int y){
-        g.setColor(Color.black);
-        g.fillOval(x,y,50,50);
+    public void paint(Graphics g){
+        super.paint(g);
         g.setColor(Color.yellow);
-        g.fillOval(x,y,48,48);
+        g.fillOval(X,Y,48,48);
         g.setColor(Color.black);
-        g.drawString(""+this.getValor(), x+10, y+20);
+        g.drawString(""+this.getValor(), X+10, Y+20);
+    }
+    public void setXY(int x, int y){
+        X = x;
+        Y = y;
+        
     }
 }
